3e74779397cf2cfdd9af56d0ec817a75e9de8526
JayathiBS <jayathibaddewatta@gmail.com>
2025-11-15 20:12:56 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/JayathiBS/jayathi_portfolio",  // <-- IMPORTANT
+  base: "/jayathi_portfolio/",  // <-- IMPORTANT
 })
